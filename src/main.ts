ec6cc51d9a418bf807de80af003414c1100337f8
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2024-05-21 22:44:20 +0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
+
+  const PORT = process.env.PORT || 5000;
+  await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}/api`);
+}
+bootstrap();
